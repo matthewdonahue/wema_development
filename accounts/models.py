aa5d6d9f8a8b6def08fa09179ff6a689b76911cc
matthew donahue <donahue88888888@gmail.com>
2021-06-08 09:12:06 -0400

    Enter

---
+++ accounts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserPersona(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    normalized_name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
+
+
+# Create your models here.
+class UserProfile(models.Model):
+    # owner
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+
+    is_full_name_displayed = models.BooleanField(default=True)
+
+    #details
+    gup_num = models.CharField(max_length=15, default='')
+    persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
+
--- wema/wsgi.py
+++ wema/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wema.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wema.settings")
 
 application = get_wsgi_application()
